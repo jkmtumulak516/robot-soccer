ef44d0599d7734c59d811fe073a2f2a360441a0a
Jose Karol Tumulak <tumulakjkm@gmail.com>
2017-08-08 22:06:23 +0800

    created initial UI

--- RobotSoccer/RobotSoccer/MainForm.Designer.cs
+++ RobotSoccer/RobotSoccer/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RobotSoccer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SoccerField = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // Form1
+            // SoccerField
+            // 
+            this.SoccerField.AutoSize = true;
+            this.SoccerField.BackColor = System.Drawing.Color.White;
+            this.SoccerField.Location = new System.Drawing.Point(12, 12);
+            this.SoccerField.Margin = new System.Windows.Forms.Padding(15);
+            this.SoccerField.Name = "SoccerField";
+            this.SoccerField.Size = new System.Drawing.Size(1160, 837);
+            this.SoccerField.TabIndex = 0;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1184, 861);
+            this.Controls.Add(this.SoccerField);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Robot Soccer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel SoccerField;
     }
 }
 
